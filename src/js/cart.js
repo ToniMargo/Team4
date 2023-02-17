dc41684e00bfa60a8ae4e28897ee23c9804460a1
Noctumbras <stephen64s@byui.edu>
2023-02-16 20:49:49 -0600

    there is an error in the browser with this, but it appears to function correctly

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,8 +1,22 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { getCartContent, loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.js";
 
 loadHeaderFooter();
 const element = document.querySelector(".product-list");
+const totalFooter = document.querySelector(".cart-footer");
+const totalCost = document.querySelector(".cart-total");
 const cart = new ShoppingCart(element);
 
-cart.init();
+cart.init().then(async (create_total) => {
+  if (element.innerHTML.trim() !== "") {
+    totalFooter.removeAttribute("hidden");
+    const contents = await getCartContent();
+    let amount = 0;
+
+    for (let x = 0; x < contents.length; x++) {
+      amount += contents[x].FinalPrice;
+    }
+
+    totalCost.innerHTML = `Total: $${amount}`;
+  }
+});
