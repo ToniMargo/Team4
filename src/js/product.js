fdeab68a36e8f660b66a4713041177f868a8a072
antoniomartinez <mar20126@byui.edu>
2023-02-18 13:55:38 -0700

    Ponder: Team Activity 6

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,11 @@
 import { getParam, loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 loadHeaderFooter();
 
+const dataSource = new ExternalServices("tents");
 const productId = getParam("product");
-const dataSource = new ProductData("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -5,35 +5,6 @@ export function qs(selector, parent = document) {
 // or a more concise version if you are into that sort of thing:
 // export const qs = (selector, parent = document) => parent.querySelector(selector);
 
-// Generates a random combination of characters for each localStorage item's key
-// All credits of this function go to Corylus: https://stackoverflow.com/questions/59530970/how-to-get-a-random-number-letter-combination-in-javascript
-export function generateKey() {
-  var characters =
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    key = "";
-  for (var i = 0, n = characters.length; i < 62; ++i) {
-    key += characters.charAt(Math.floor(Math.random() * n));
-  }
-  return key;
-}
-
-export function getCartContent() {
-  const cartItems = [];
-  let itemKey = "";
-  for (let i = 0; i < localStorage.length; i++) {
-    itemKey = localStorage.key(i);
-    cartItems.push(getLocalStorage(itemKey));
-  }
-  // const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  // document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  return cartItems;
-}
-
-export function limitNumberoFItems(list, value) {
-  let limited = list.slice(value);
-  return limited;
-}
-
 // retrieve data from localstorage
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
@@ -42,19 +13,8 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
-// set a listener for both touchend and click
-export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
-    event.preventDefault();
-    callback();
-  });
-  qs(selector).addEventListener("click", callback);
-}
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const product = urlParams.get("product");
-
+// helper to get parameter strings
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
@@ -62,6 +22,7 @@ export function getParam(param) {
   return product;
 }
 
+// function to take a list of objects and a template and insert the objects as HTML into the DOM
 export function renderListWithTemplate(
   templateFn,
   parentElement,
@@ -77,6 +38,7 @@ export function renderListWithTemplate(
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
 
+// function to take an optional object and a template and insert the objects as HTML into the DOM
 export function renderWithTemplate(template, parentElement, data, callback) {
   parentElement.insertAdjacentHTML("afterbegin", template);
   //if there is a callback...call it and pass data
@@ -91,12 +53,22 @@ async function loadTemplate(path) {
   return template;
 }
 
+// function to dynamically load the header and footer into a page
 export async function loadHeaderFooter() {
-  const headerCode = await loadTemplate("../partials/header.html");
-  const footerCode = await loadTemplate("../partials/footer.html");
-  const header = document.querySelector("header");
-  const footer = document.querySelector("footer");
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
 
-  renderWithTemplate(headerCode, header);
-  renderWithTemplate(footerCode, footer);
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
+
+// set a listener for both touchend and click
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
 }
